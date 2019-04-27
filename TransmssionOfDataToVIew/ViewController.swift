592eb85e6d3e44aaeb4464edbeebd344e2f85587
Aleksandr <suprinets87@gmail.com>
2019-04-27 22:12:21 +0300

    Initial Commit

---
+++ TransmssionOfDataToVIew/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TransmssionOfDataToVIew
+//
+//  Created by Aleksandr Suprunets on 4/27/19.
+//  Copyright © 2019 Aleksandr Suprunets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
